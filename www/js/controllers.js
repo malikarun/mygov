e2c736890289b545b8e4fcfa75bd617806412195
Arun Malik <akmalik@ymail.com>
2014-09-30 21:50:09 +1000

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,21 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+})
+
+.controller('GroupCtrl', function($scope) {
+})
+
+.controller('DiscussCtrl', function($scope) {
+});
